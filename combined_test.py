9662b3dd39cf2164233099b794f7541bc4564580
nathik <nathikazad@gmail.com>
2018-08-15 20:18:05 -0700

    Added file writing

--- combined_test.py
+++ combined_test.py
@@ -4,6 +4,7 @@ import json
 import serial
 from threading import Thread
 import binascii
+import time
 
 
 def convert(number, sign, len, fp_index):
@@ -26,72 +27,59 @@ def convert(number, sign, len, fp_index):
   return ret;
 
 
-sbc_stream = "\0" * 100
+
+engine_data = {'FUEL': 0, 'PWR': 0, 'RPM': 0, 'MANP': 0, 'FUELP': 0, 
+      'OILP': 0, 'OILT': 0, 'CHT1': 0, 'CHT2': 0,  'CHT3': 0, 
+      'CHT4': 0, 'CHT5': 0, 'CHT6': 0, 'EGT1': 0, 'EGT2': 0, 
+      'EGT3': 0, 'EGT4': 0, 'EGT5': 0, 'EGT6': 0 }
 
 port = serial.Serial("/dev/serial0", baudrate=19200, timeout=3.0)
 
-# TODO:add 100ms delay
-# TODO:create json here
-# TODO:save to file
+# TODO:add 100ms delay DONE?
+# TODO:create json here DONE?
+# TODO:save to file DONE?
 # TODO:Check for additional sync bytes
+# TODO: Handle case where no read in 3 seonds
 def update_sbc_stream():
-  global sbc_stream
+  filename  = time.strftime("log/%Y%m%d-%H%M%S")
+  file = open(filename,"w+")
+  global engine_data
   while True:
+    time.sleep(0.1)
     if port.in_waiting:
       sbc_stream = port.read_until(b'\xfc')
+      engine_data = {}
+      engine_data['FUEL'] = 0
+      engine_data['PWR'] = convert(sbc_stream[3:5], False, 16, 9)
+      engine_data['RPM'] = convert(sbc_stream[7:9], False, 16, 4)
+      engine_data['MANP'] = convert(sbc_stream[9:11], False, 16, 8)
+      engine_data['FUELP'] = convert(sbc_stream[11:13], False, 16, 9)
+      engine_data['OILP'] = convert(sbc_stream[47:49], False, 16, 9)
+      engine_data['OILT'] = convert(sbc_stream[49:51], False, 16, 9)
+      engine_data['CHT1'] = convert(sbc_stream[15:17], False, 16, 6)
+      engine_data['CHT2'] = convert(sbc_stream[17:19], False, 16, 6)
+      engine_data['CHT3'] = convert(sbc_stream[19:21], False, 16, 6)
+      engine_data['CHT4'] = convert(sbc_stream[21:23], False, 16, 6)
+      engine_data['CHT5'] = convert(sbc_stream[23:25], False, 16, 6)
+      engine_data['CHT6'] = convert(sbc_stream[25:27], False, 16, 6)
+      engine_data['EGT1'] = convert(sbc_stream[31:33], False, 16, 4)
+      engine_data['EGT2'] = convert(sbc_stream[33:35], False, 16, 4)
+      engine_data['EGT3'] = convert(sbc_stream[35:37], False, 16, 4)
+      engine_data['EGT4'] = convert(sbc_stream[37:39], False, 16, 4)
+      engine_data['EGT5'] = convert(sbc_stream[39:41], False, 16, 4)
+      engine_data['EGT6'] = convert(sbc_stream[41:43], False, 16, 4)
+      file.write(time.strftime("%Y/%m/%d-%H:%M:%S"))
+      file.write(json.dumps(engine_data))
+      file.write("\n")
 
 
 app = Flask(__name__)
 api = Api(app)
 
-num = 0.0
 class Engine(Resource):
     def get(self):
-        global sbc_stream
-        global num 
-        engine_data = {}
-        engine_data['FUEL'] = 0
-        engine_data['PWR'] = convert(sbc_stream[3:5], False, 16, 9)
-        engine_data['RPM'] = convert(sbc_stream[7:9], False, 16, 4)
-        engine_data['MANP'] = convert(sbc_stream[9:11], False, 16, 8)
-        engine_data['FUELP'] = convert(sbc_stream[11:13], False, 16, 9)
-        engine_data['OILP'] = convert(sbc_stream[47:49], False, 16, 9)
-        engine_data['OILT'] = convert(sbc_stream[49:51], False, 16, 9)
-        engine_data['CHT1'] = convert(sbc_stream[15:17], False, 16, 6)
-        engine_data['CHT2'] = convert(sbc_stream[17:19], False, 16, 6)
-        engine_data['CHT3'] = convert(sbc_stream[19:21], False, 16, 6)
-        engine_data['CHT4'] = convert(sbc_stream[21:23], False, 16, 6)
-        engine_data['CHT5'] = convert(sbc_stream[23:25], False, 16, 6)
-        engine_data['CHT6'] = convert(sbc_stream[25:27], False, 16, 6)
-        engine_data['EGT1'] = convert(sbc_stream[31:33], False, 16, 4)
-        engine_data['EGT2'] = convert(sbc_stream[33:35], False, 16, 4)
-        engine_data['EGT3'] = convert(sbc_stream[35:37], False, 16, 4)
-        engine_data['EGT4'] = convert(sbc_stream[37:39], False, 16, 4)
-        engine_data['EGT5'] = convert(sbc_stream[39:41], False, 16, 4)
-        engine_data['EGT6'] = convert(sbc_stream[41:43], False, 16, 4)
+        global engine_data
 
-        # steps = 100
-        # engine_data['FUEL'] = num/steps * 100
-        # engine_data['PWR'] = num/steps * 100
-        # engine_data['RPM'] = num/steps * 2700
-        # engine_data['MANP'] = num/steps * 45
-        # engine_data['FUELP'] = num/steps * 120
-        # engine_data['OILP'] = num/steps * 110
-        # engine_data['OILT'] = num/steps * 250
-        # engine_data['CHT1'] = num/steps * 500
-        # engine_data['CHT2'] = num/steps * 500
-        # engine_data['CHT3'] = num/steps * 500
-        # engine_data['CHT4'] = num/steps * 500
-        # engine_data['CHT5'] = num/steps * 500
-        # engine_data['CHT6'] = num/steps * 500
-        # engine_data['EGT1'] = num/steps * 1800
-        # engine_data['EGT2'] = num/steps * 1800
-        # engine_data['EGT3'] = num/steps * 1800
-        # engine_data['EGT4'] = num/steps * 1800
-        # engine_data['EGT5'] = num/steps * 1800
-        # engine_data['EGT6'] = num/steps * 1800
-        # if num < steps:
-        #   num = num + 1
         return Response(json.dumps(engine_data), status=200, mimetype='application/json')
 
 @app.route("/")
@@ -104,4 +92,4 @@ api.add_resource(Engine, '/engine')
 if __name__ == "__main__":
   thread = Thread(target = update_sbc_stream)
   thread.start()
-  app.run()
+  app.run(port=5005)
